3926cebbe78c6e7f6f833034a84aeb8ace98cca8
Brandon Valosek <bvalosek@gmail.com>
2013-07-31 10:42:46 -0500

    More IS generated code

--- infusionsoft/Table.js
+++ infusionsoft/Table.js
@@ -1,4 +1,5 @@
 var typedef = require('typedef');
+var _       = require('underscore');
 
 module.exports = Table = typedef
 
@@ -9,13 +10,13 @@ module.exports = Table = typedef
     __ondefine__: function(C, hash)
     {
         var FIELDS = [];
-        _(typedef.signature(C)).each(function(info, key) {
+        _(C.__signature__).each(function(info, key) {
             if (info.decorations.FIELD)
                 FIELDS.push(key);
         });
 
         if (FIELDS.length)
             C.FIELDS = FIELDS;
-    },
+    }
 
 });
